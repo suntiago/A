599878d83ff3678e2b0caa64946514ce7da57c44
chenjunwei <chenjunwei@viroyal.cn>
2019-05-28 11:30:10 +0800

    节假日、音频播放工具类添加

---
+++ a/src/main/java/com/suntiago/a/HolidayUtil.java
@@ -0,0 +1,29 @@
+package com.suntiago.a;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * @author chenjunwei
+ * @desc
+ * @date 2019/5/28
+ */
+public class HolidayUtil {
+
+  private static Map<String, String> holiday = new HashMap<>();
+
+  static {
+    holiday.put("01/01", "元旦");
+    holiday.put("05/01", "劳动节");
+    holiday.put("10/01", "国庆节");
+    holiday.put("正月初一", "春节");
+    holiday.put("五月初五", "端午节");
+    holiday.put("八月十五", "中秋节");
+  }
+
+  public static String getHoliday(String key) {
+
+    return holiday.get(key);
+  }
+}
