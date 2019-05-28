ff3e75f98911d04ee3b01c7693fd7718e4037e02
chenjunwei <chenjunwei@viroyal.cn>
2019-05-28 13:06:11 +0800

    节假日、音频播放工具类添加

--- a/src/main/java/com/suntiago/a/MediaPlayUtil.java
+++ a/src/main/java/com/suntiago/a/MediaPlayUtil.java
@@ -6,34 +6,39 @@ import android.media.MediaPlayer;
 
 import java.io.IOException;
 
+/**
+ * @author chenjunwei
+ * @desc 音频播放
+ * @date 2019/5/28
+ */
 public class MediaPlayUtil {
-    private static MediaPlayUtil mMediaPlayUtil;
-    private MediaPlayer mMediaPlayer;
-    private static final String TAG = "MediaPlayUtil";
+  private static MediaPlayUtil mMediaPlayUtil;
+  private MediaPlayer mMediaPlayer;
+  private static final String TAG = "MediaPlayUtil";
 
-    public static MediaPlayUtil getInstance() {
-        if (mMediaPlayUtil == null) {
-            mMediaPlayUtil = new MediaPlayUtil();
-        }
-        return mMediaPlayUtil;
+  public static MediaPlayUtil getInstance() {
+    if (mMediaPlayUtil == null) {
+      mMediaPlayUtil = new MediaPlayUtil();
     }
+    return mMediaPlayUtil;
+  }
 
-    private MediaPlayUtil() {
-        mMediaPlayer = new MediaPlayer();
-    }
+  private MediaPlayUtil() {
+    mMediaPlayer = new MediaPlayer();
+  }
 
-    public void play(Context c, int id) {
-        mMediaPlayer.stop();
-        AssetFileDescriptor afd = c.getResources().openRawResourceFd(id);
-        try {
-            mMediaPlayer.reset();
-            mMediaPlayer.setDataSource(afd.getFileDescriptor(),
-                    afd.getStartOffset(), afd.getLength());
-            afd.close();
-            mMediaPlayer.prepare();
-            mMediaPlayer.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  public void play(Context c, int id) {
+    mMediaPlayer.stop();
+    AssetFileDescriptor afd = c.getResources().openRawResourceFd(id);
+    try {
+      mMediaPlayer.reset();
+      mMediaPlayer.setDataSource(afd.getFileDescriptor(),
+              afd.getStartOffset(), afd.getLength());
+      afd.close();
+      mMediaPlayer.prepare();
+      mMediaPlayer.start();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
